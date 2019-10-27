d38fcff0400e9bea944721b7f4c8caf83b17b56b
Federico Castañares <federico.castanares@gmail.com>
2019-10-27 19:47:08 -0300

    arreglos a funcion

--- wp-content/themes/hestia-child/app.js
+++ wp-content/themes/hestia-child/app.js
@@ -7,6 +7,9 @@ async function cargarPosts() {
         // Para produccion poner window.local
     let url = `http://54.153.122.87/wp-json/wp/v2/posts`;
     let respuesta = await fetch(url)
-    const datos = await respuesta.json();
-    console.log(datos);
+    const posts = await respuesta.json();
+    return posts;
 }
+
+cargarPosts()
+    .then (posts => console.log(posts));
