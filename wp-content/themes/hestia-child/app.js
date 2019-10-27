2a41d5fc478caf0fa9c9712439f75b6da7617f72
Federico Castañares <federico.castanares@gmail.com>
2019-10-27 19:43:32 -0300

    funcion async

--- wp-content/themes/hestia-child/app.js
+++ wp-content/themes/hestia-child/app.js
@@ -3,7 +3,7 @@ document.getElementById('hestia-about ').addEventListener('DOMContentLoaded', ca
 
 
 
-function cargarPosts() {
+async function cargarPosts() {
         // Para produccion poner window.local
     let url = `http://54.153.122.87/wp-json/wp/v2/posts`;
     let respuesta = await fetch(url)
