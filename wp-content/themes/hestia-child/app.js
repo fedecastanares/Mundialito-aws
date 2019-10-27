92deec51e7224486b77b06de634f5106ebcecf9a
Federico Castañares <federico.castanares@gmail.com>
2019-10-27 19:41:31 -0300

    tenia un espacio al final

--- wp-content/themes/hestia-child/app.js
+++ wp-content/themes/hestia-child/app.js
@@ -1,4 +1,4 @@
-document.getElementById('hestia-about').addEventListener('DOMContentLoaded', cargarPosts);
+document.getElementById('hestia-about ').addEventListener('DOMContentLoaded', cargarPosts);
 
 
 
