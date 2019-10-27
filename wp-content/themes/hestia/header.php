6709a34aad8ab50d2af4fa9b33cf3e11206dfcac
Federico Castañares <federico.castanares@gmail.com>
2019-10-27 01:49:02 -0300

    sacando app del tema principal

--- wp-content/themes/hestia/header.php
+++ wp-content/themes/hestia/header.php
@@ -33,7 +33,6 @@ if ( (bool) $hide_top_bar === false ) {
 <head>
 	<meta charset='<?php bloginfo( 'charset' ); ?>'>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<script src="./app.js" type="text/javascript"></script>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
